d5bd8bab3bba9ba6309be4039ab36867ea533db1
Sushant Mishra <mishrasushant1901@gmail.com>
2021-10-05 22:41:23 +0530

    Patterns

---
+++ src/com/company/equilateral_triangle.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class equilateral_triangle {
+    public static void main(String[] args) {
+        for(int i=1;i<=4;i++){
+            for(int j=1;j<=4-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("* ");
+
+            }
+            System.out.println("");
+        }
+
+    }
+}
---
+++ src/com/company/half_pyramid.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class half_pyramid {
+    public static void main(String[] args) {
+        for(int i=0;i<=5;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
